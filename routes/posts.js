a192e966b683bc5ef778038f4d05772f9b3d887e
Alexandria <walkerciara24@gmail.com>
2024-10-30 20:16:53 -0400

    Refresh data

--- routes/posts.js
+++ routes/posts.js
@@ -1,80 +1,22 @@
-const express = require("express");
-const router = express.Router();
-
-const posts = require("../data/posts");
-const error = require("../utilities/error");
-
-router
-  .route("/")
-  .get((req, res) => {
-    const links = [
-      {
-        href: "posts/:id",
-        rel: ":id",
-        type: "GET",
-      },
-    ];
-
-    res.json({ posts, links });
-  })
-  .post((req, res, next) => {
-    if (req.body.userId && req.body.title && req.body.content) {
-      const post = {
-        id: posts[posts.length - 1].id + 1,
-        userId: req.body.userId,
-        title: req.body.title,
-        content: req.body.content,
-      };
-
-      posts.push(post);
-      res.json(posts[posts.length - 1]);
-    } else next(error(400, "Insufficient Data"));
-  });
-
-router
-  .route("/:id")
-  .get((req, res, next) => {
-    const post = posts.find((p) => p.id == req.params.id);
-
-    const links = [
-      {
-        href: `/${req.params.id}`,
-        rel: "",
-        type: "PATCH",
-      },
-      {
-        href: `/${req.params.id}`,
-        rel: "",
-        type: "DELETE",
-      },
-    ];
-
-    if (post) res.json({ post, links });
-    else next();
-  })
-  .patch((req, res, next) => {
-    const post = posts.find((p, i) => {
-      if (p.id == req.params.id) {
-        for (const key in req.body) {
-          posts[i][key] = req.body[key];
-        }
-        return true;
-      }
-    });
-
-    if (post) res.json(post);
-    else next();
-  })
-  .delete((req, res, next) => {
-    const post = posts.find((p, i) => {
-      if (p.id == req.params.id) {
-        posts.splice(i, 1);
-        return true;
-      }
-    });
-
-    if (post) res.json(post);
-    else next();
-  });
-
-module.exports = router;
+// Keeps all the posts users made
+// Each post has an id and shows which user wrote it (userId)
+const posts = [
+  {
+    id: 1,
+    userId: 1, 
+    title: "est et quae odit qui non",
+    content: "similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero",
+  },
+  {
+    id: 2,
+    userId: 1,  
+    title: "quasi id et eos tenetur aut quo autem",
+    content: "eum sed dolores ipsam sint possimus debitis occaecati\ndebitis qui qui et\nut placeat enim earum aut odit facilis\nconsequatur suscipit necessitatibus rerum sed inventore temporibus consequatur",
+  },
+  {
+    id: 3,
+    userId: 2, 
+    title: "delectus ullam et corporis nulla voluptas sequi",
+    content: "non et quaerat ex quae ad maiores\nmaiores recusandae totam aut blanditiis mollitia quas illo\nut voluptatibus voluptatem\nsimilique nostrum eum",
+  }
+];
