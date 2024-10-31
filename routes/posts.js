d7ff4ac53e3473451a0a46120cb8c9286b93bcad
Alexandria <walkerciara24@gmail.com>
2024-10-31 00:54:23 -0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -16,3 +16,17 @@ router.get("/", (req, res) => {
   ];
   res.json({ posts, links });
 });
+// When someone wants to create a NEW post
+router.post("/", (req, res, next) => { 
+  // Make sure they send the info needed
+  if (req.body.userId && req.body.title && req.body.content) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,  // Give it the next available ID
+      userId: req.body.userId,
+      title: req.body.title,
+      content: req.body.content,
+    };
+    posts.push(post);
+    res.json(posts[posts.length - 1]);
+  } else next(error(400, "Insufficient Data"));
+});
